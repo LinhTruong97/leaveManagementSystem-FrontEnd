4588ba6784784ec38d7e90e4939fe93420b325b2
Linh Truong <hoailinhtruong12397@gmail.com>
2023-11-28 17:49:52 +0700

    [feature]: update firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
-import { FIREBASE_CONFIG, VAPID_KEY } from "./firebaseConfig";
 
-const firebaseConfig = FIREBASE_CONFIG;
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const messaging = getMessaging(firebaseApp);
-
 // Request a new FCM token
 export const requestForToken = async () => {
   try {
     const currentToken = await getToken(messaging, {
-      vapidKey: VAPID_KEY,
+      vapidKey: process.env.REACT_APP_VAPID_KEY,
     });
     if (currentToken) {
       console.log("Current FCM token:", currentToken);
